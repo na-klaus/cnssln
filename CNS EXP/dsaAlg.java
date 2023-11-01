4ebb2ea2e9517ea791a2667d352f2a9d5503c6de
na-klaus <78178240+na-klaus@users.noreply.github.com>
2023-11-01 17:25:00 +0530

    Update dsaAlg.java

--- CNS EXP/dsaAlg.java
+++ CNS EXP/dsaAlg.java
@@ -4,7 +4,6 @@ class dsaAlg
 {
 final static BigInteger one = new BigInteger("1");
 final static BigInteger zero = new BigInteger("0");
-/* incrementally tries for next prime */
 public static BigInteger getNextPrime(String ans)
 {
 BigInteger test = new BigInteger(ans);
@@ -28,7 +27,6 @@ n = n.divide(start);
 }
 return n;
 }
-/* finds a generator mod p */
 public static BigInteger getGen(BigInteger p,BigInteger q,Random r)
 {
 BigInteger h = new BigInteger(p.bitLength(), r);
@@ -38,32 +36,24 @@ return h.modPow((p.subtract(one)).divide(q), p);
 public static void main (String[] args) throws java.lang.Exception
 {
 Random randObj = new Random();
-/* establish the global public key components */
 BigInteger p = getNextPrime("10601");
-/* approximate prime */
 BigInteger q = findQ(p.subtract(one));
 BigInteger g = getGen(p,q,randObj);
-/* public key components */
 System.out.println("simulation of Digital Signature Algorithm");
 System.out.println("global public key components are:");
 System.out.println("p is: " + p);
 System.out.println("q is: " + q);
 System.out.println("g is: " + g);
-/* find the private key */
 BigInteger x = new BigInteger(q.bitLength(), randObj);
 x = x.mod(q);
-/* corresponding public key */
 BigInteger y = g.modPow(x,p);
-/* random value message */
 BigInteger k = new BigInteger(q.bitLength(), randObj);
 k = k.mod(q);
-/* randomly generated hash value and digital signature */
 BigInteger r = (g.modPow(k,p)).mod(q);
 BigInteger hashVal = new BigInteger(p.bitLength(), randObj);
 BigInteger kInv = k.modInverse(q);
 BigInteger s = kInv.multiply(hashVal.add(x.multiply(r)));
 s = s.mod(q);
-/* secret information */
 System.out.println("secret information are:");
 System.out.println("x (private) is: " + x);
 System.out.println("k (secret) is: " + k);
@@ -72,7 +62,6 @@ System.out.println("h (rndhash) is: " + hashVal);
 System.out.println("generating digital signature:");
 System.out.println("r is : " + r);
 System.out.println("s is : " + s);
-/* verify the digital signature */
 BigInteger w = s.modInverse(q);
 BigInteger u1 = (hashVal.multiply(w)).mod(q);
 BigInteger u2 = (r.multiply(w)).mod(q);
